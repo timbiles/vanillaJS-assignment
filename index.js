36df69c0c5adcc1044b176be9c5f1a92ad2ec759
Tim Biles <timbilestimbiles@gmail.com>
2019-01-29 22:15:03 -0600

    setup, first 2 steps

---
+++ index.js
@@ -0,0 +1,69 @@
+// let turn = 'X';
+
+const play = () => {
+  const player = document.getElementById('player');
+
+  if (player.innerText === 'X') {
+    player.innerText = 'O';
+  } else {
+    player.innerText = 'X';
+  }
+};
+
+// let turn = 'X';
+// let board = [];
+
+// const play = val => {
+//     const player = document.getElementById('player')
+//   const element = document.getElementById(val);
+//   let result;
+
+//   board[val] = turn
+//   if (turn === 'X') {
+//     element.innerText = 'X';
+//     turn = 'O'
+//   } else {
+//     element.innerText = 'O';
+//     turn = 'X'
+//   }
+//   player.innerText = turn
+
+//   board.map((elem,i) => {
+//     if(elem) {
+//         if (
+//             (board[0] === board[4] && board[0] === board[8]) ||
+//             (board[2] === board[4] && board[2] === board[6])
+//           ) {
+//             result = board[4]
+//           } else if (
+//             (i === 0 || i === 1 || i === 2) &&
+//             (board[i] === board[i + 3] && board[i] === board[i + 6])
+//           ) {
+//             result = elem
+//           } else if (
+//             (i === 0 || i === 3 || i === 6) &&
+//             (board[i] === board[i + 1] && board[i] === board[i + 2])
+//           ) {
+//             result = elem
+//           }
+//     }
+// })
+
+// if (result) {
+//     window.alert(`${result} is the winner!`)
+//     resetBoard()
+// }
+
+// console.log(board)
+
+// };
+
+// const resetBoard = () => {
+// const tag = document.getElementsByTagName('td')
+
+// for(let i =0; i<tag.length; i++){
+//     tag[i].innerText = ''
+// }
+// board = []
+// turn = 'X'
+// }
